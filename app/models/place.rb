7189cf0390f92cfeb781601c316f379e00415924
bradzhang1128@gmail.com <deadbred3@gmail.com>
2017-04-24 10:11:36 -0400

    add aggregation framework and geolocation queries

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,7 +5,7 @@ class Place
 		@id = params[:_id].to_s
 		@formatted_address = params[:formatted_address]
 		@location = Point.new(params[:geometry][:geolocation])
-		@address_components = params[:address_components].map { |a| AddressComponent.new(a) } 
+		@address_components = params[:address_components].map { |a| AddressComponent.new(a) } if !params[:address_components].nil?
 	end
 
 	def self.mongo_client
@@ -45,5 +45,85 @@ class Place
 	def destroy
 		id = BSON::ObjectId.from_string(@id)
 		self.class.collection.find(_id: id).delete_one
+	end
+
+	def self.get_address_components(sort=nil, offset=0, limit=nil)
+		prototype = [ 
+			{
+				:$unwind => "$address_components"
+			},
+		    {
+		    	:$project => { :_id => 1, :address_components => 1, :formatted_address => 1, :'geometry.geolocation' => 1}
+		    }
+		]
+
+		
+		prototype << {:$sort => sort} if !sort.nil?
+		prototype << {:$skip => offset} if offset != 0
+		prototype << {:$limit => limit} if !limit.nil?
+
+		collection.find.aggregate(prototype)
+
+	end
+
+	def self.get_country_names
+		prototype = [
+			{
+				:$unwind => "$address_components"
+			}, 
+		    {
+		    	:$project => { :'address_components.long_name' => 1, :'address_components.types' => 1}
+		    },
+		    {
+		    	:$match => { :'address_components.types' => "country"}
+		    },
+		    {
+		    	:$group => { :_id => "$address_components.long_name"}
+		    }
+		]
+
+		result = collection.find.aggregate(prototype)
+
+		result.to_a.map { |h| h[:_id]}
+	end
+
+	def self.find_ids_by_country_code(country_code)
+		prototype = [
+			{
+				:$match => { :'address_components.types' => "country", 
+					         :'address_components.short_name' => country_code }
+			},
+		    {
+		    	:$project => { :_id => 1}
+		    }
+		]
+
+		result = collection.find.aggregate(prototype)
+
+		result.to_a.map { |doc| doc[:_id].to_s}
+	end
+
+	def self.create_indexes
+		collection.indexes.create_one( :'geometry.geolocation' => Mongo::Index::GEO2DSPHERE)
+	end
+
+	def self.remove_indexes
+		collection.indexes.drop_one('geometry.geolocation_2dsphere')
+	end
+
+	def self.near(point, max_meters = nil)
+		collection.find( :'geometry.geolocation' =>
+		    {
+		    	:$near => {
+		    		:$geometry => point.to_hash,
+		    		:$maxDistance => max_meters
+		    	}
+
+			})
+	end
+
+	def near(max_meters = nil)
+		result = self.class.near(@location, max_meters)
+		self.class.to_places(result)
 	end
 end
